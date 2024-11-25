fc8043a20c934985d4fd372067611f622abef986
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-11-25 10:41:16 +0700

    training

--- Chatbot-Model/input.py
+++ Chatbot-Model/input.py
@@ -1,18 +1,15 @@
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+load_in_4bit = False # Use 4bit quantization to reduce memory usage. Can be False.
 
-from unsloth import FastLanguageModel
-
-
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
 def chat():
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "lora_model", 
-        max_seq_length = max_seq_length,
-        dtype = dtype,
-        load_in_4bit = True,
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        "D:\ProgrammingProject\chatbot\Chatbot-Model\lora_model", # YOUR MODEL YOU USED FOR TRAINING
+        load_in_4bit = load_in_4bit,
     )
-    FastLanguageModel.for_inference(model) 
+    tokenizer = AutoTokenizer.from_pretrained("D:\ProgrammingProject\chatbot\Chatbot-Model\lora_model")
 
     alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
@@ -29,7 +26,7 @@ def chat():
             "", # input - leave this blank
             "", # output - leave this blank for generation!
         )
-    ], return_tensors = "pt").to("cuda")
+    ], return_tensors = "pt")
 
     from transformers import TextStreamer
     text_streamer = TextStreamer(tokenizer)
